f2e9dd3c42d2d00b16d1a18fd8553a617f5bc013
Bas van Beek <b.f.van.beek@vu.nl>
2019-10-10 19:39:52 +0200

    Update

--- assertionlib/__init__.py
+++ assertionlib/__init__.py
@@ -1,7 +1,9 @@
 from .__version__ import __version__
 
 from .manager import assertion, AssertionManager
+from .functions import load_readme
 
+__doc__ = load_readme()
 __version__ = __version__
 __author__ = "B. F. van Beek"
 __email__ = 'b.f.van.beek@vu.nl'
